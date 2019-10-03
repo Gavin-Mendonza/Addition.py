3c00af61666952b858e3f5ebb93f70276f7b455e
Gavin-Mendonza <53964233+Gavin-Mendonza@users.noreply.github.com>
2019-10-03 08:22:29 +0530

    Add files via upload

---
+++ PythonProgram(F089).py
@@ -0,0 +1,12 @@
+def addition():
+    print("Program to add two numbers:-")
+    num1 = int(input("enter a number: "))
+    num2 = int(input("enter second number: "))
+
+    ans = num1+num2
+
+    print("The addition of the two numbers are: ", ans)
+
+
+            
+addition()
